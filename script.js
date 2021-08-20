5fcde2186338bec04356f634dc9f76fe28487c3e
Sam Dollar <sdollar1@gmail.com>
2021-08-20 14:18:33 -0500

    more tests and comments

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-(function() {
+// (function() {
     let calculator = {
         init: function() {
             this.cacheDom();
+            this.clear();
         },
 
         cacheDom: function() {
-            const numberBtn = document.querySelectorAll('.num');
-            const decimalBtn = document.getElementById('decimal');
-            const operandBtn = document.querySelectorAll('.operand');
-            const currentResult = document.getElementById('current-result');
-            const previousResult = document.getElementById('previous-result');
-            
-            currentResult.innerText = 'testing'; //this replaces the formatting for original p element test, so will need to add 
-            //numbers that appear in results to a class
+            let numberBtn = document.querySelectorAll('.num');
+            let decimalBtn = document.getElementById('decimal');
+            let operandBtn = document.querySelectorAll('.operand');
+            let clearBtn = document.getElementById('clear');
+            let deleteBtn = document.getElementById('del');
+            let equalsBtn = document.querySelector('.equals');
+            let currentResult = document.getElementById('current-result');
+            let previousResult = document.getElementById('previous-result');
+            let currentResultText = document.querySelector('.display-current');
+            let previousResultText = document.querySelector('.display-previous');
+            currentResultText.innerText = '30'; //currentResult displays w/out formatting, this keeps padding etc
+
         },
 
         operate: function(val1, val2, operator) {
+            //make seperate functions for add, subtract, multiply, divide, that take 2 numbers and an operator
+        },
 
+        clear: function() {
+            // this.currentResultText.innerText = ''; //this variable remains undefined if called from here.
+            // this.previousResultText.innerText = '';
+            console.log('clear');
         },
     }
     calculator.init();
-})();
+    
+// })();
